e83429b1d2c92ea2e4c0f3b4753d99438e6311a7
Satvik gupta <124979868+Satvik-knolx@users.noreply.github.com>
2023-03-15 02:03:17 +0530

    Update factorialUsingRecursion.scala

--- scala-KUP-assignments/factorialRecursion/src/main/scala/factorialUsingRecursion.scala
+++ scala-KUP-assignments/factorialRecursion/src/main/scala/factorialUsingRecursion.scala
@@ -6,12 +6,10 @@ object factorialUsingRecursion extends App {
 
   @tailrec  //this annotation is used to verify whether the method is using tail recursion or not
   def recursiveFactorial(factorialNumber: Int, acc: Int): Int = { //acc is an accumulator which is used to store the immediate result of computation
-    {
       if (factorialNumber == 0)
         acc
       else
         recursiveFactorial(factorialNumber - 1, acc * factorialNumber)
-    }
   }
 
 }
